1665ed62fcabda453fcdd2f9147016d7dbaac019
peskyji <peskyji.com>
2023-09-06 19:28:37 +0530

    large file now will be fetched from g-drive instead of github due to git lfs limit issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # =========================== Imporitng required libraries ==============================================================================================
 import streamlit as st
 import pandas as pd
-import joblib
+import joblib, pickle
+from urllib.request import urlopen
 from movie_counsel import *
 # =========================== Fetching dataset and ML Model ==============================================================================================
     # using st.cache_resource will run globally for the first time only. for all the sessions later results will fetched from cache.
@@ -12,7 +13,11 @@ def import_dataset_similarity_matrix():
         Note:- Make sure this function is using streamlit's cache properties to avoid loading datasets on every refresh '''
     
     movies_df = pd.read_csv("dataset/complete_dataset_with_urls.csv", keep_default_na=False) # loading movies dataset
-    similarity = joblib.load('dataset/similarity_8k.joblib') # loading similarity matrix for Recommendation
+    
+    # g_drive_url_joblib = "https://drive.google.com/uc?id=1-MxoPHx492LCSG45buqWYxycByHevZfe&confirm=t&uuid=742b2095-e0c5-4f0a-9f0e-8c9e1e0a5f5b&at=AB6BwCCo0Cg2rZlZuHXZhaAkms7S:1694004179033"
+    g_drive_url_pkl = "https://drive.google.com/uc?id=1u7NLuxrBQKaN4Fj0KS2tt98Whz927BL-&confirm=t&uuid=2525fb6b-f249-4851-8036-27d3de86dad4&at=AB6BwCA59t4_ReTmngovNU0n1vNT:1694003393913"
+    similarity = pickle.load(urlopen(g_drive_url_pkl)) # loading similarity matrix for Recommendation
+    
     return movies_df, similarity
 # ================================ Calling class movie_counsel ot build web app ====================================================================================
 

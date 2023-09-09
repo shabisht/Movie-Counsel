535c88bbf5e5ca83536e403c95cf37b29fae5dbc
peskyji <peskyji.com>
2023-09-10 02:09:44 +0530

    filter fuctionality added

--- movie_counsel.py
+++ movie_counsel.py
@@ -2,6 +2,7 @@
 import streamlit as st
 # ================================ Implementing OOPs to build WebApp ====================================================================================
 class movie_counsel:
+
     def __init__(self):
         ''' This method will set the page configs and create the session variables for the app '''
 
@@ -16,7 +17,27 @@ class movie_counsel:
             st.session_state.movie_details_selected = None
             st.session_state.movie_details_recommended = None
             st.session_state.recom_movie_details = None
-  
+            st.session_state.toogleButton = False
+            st.session_state.cntry_default = None
+            st.session_state.cntry_list = set()
+            st.session_state.genre_default = None
+            st.session_state.genre_list = set()
+            st.session_state.language_default = None
+            st.session_state.language_list = set()
+            st.session_state.kind_default = None
+            st.session_state.kind_list = set()
+            st.session_state.result_list = set()
+            st.session_state.result_default = None
+            st.session_state.apply_button = False
+            st.session_state.disabled_selectBar = False
+            st.session_state.disabled_inputBar = False
+            
+    def set_source_data(self, movies_df, similarity):
+         if "movies_df" not in st.session_state:
+            st.session_state.movies_df = movies_df
+            st.session_state.similarity = similarity
+            st.session_state.filter_df = movies_df
+
     def set_logo(self):
         ''' This methods will set the WebApp logo '''
         cols = self.create_n_columns(5)
@@ -40,19 +61,18 @@ class movie_counsel:
             st.session_state.default.update(st.session_state.SearchedMovies)
             st.session_state['default_value'] = st.session_state['default']
 
-    def create_textInput(self,movies_df):
+    def create_textInput(self):
         ''' This method creates the text_input label. It takes User's input for serching movies/series'''
-        self.input_movie = st.text_input('', value=st.session_state['input_value'], key='InputMovieName', placeholder='Enter Movie/Series Name', label_visibility='collapsed', on_change = self.todo_on_text_input_change)
+        self.input_movie = st.text_input('', value=st.session_state['input_value'], key='InputMovieName', placeholder='Enter Movie/Series Name', label_visibility='collapsed', on_change = self.todo_on_text_input_change, disabled=st.session_state.disabled_inputBar)
         if self.input_movie != "":
             self.input_movie = self.input_movie.lower()
-            index_list = movies_df[movies_df.name.str.contains(self.input_movie)].index.tolist()
+            index_list = st.session_state.movies_df[st.session_state.movies_df.name.str.contains(self.input_movie)].index.tolist()
             for x in index_list:
-                st.session_state['options'].add(movies_df.name.iloc[x])
-
-        
+                st.session_state['options'].add(st.session_state.movies_df.name.iloc[x])
+   
     def create_selectbar(self):
         ''' This method creates the multiselect label. User's selection can be seen here'''
-        self.selectbar = st.multiselect("", key="SearchedMovies", options=sorted(list(st.session_state['options'])), default=st.session_state['default_value'], max_selections=5,label_visibility='collapsed')
+        self.selectbar = st.multiselect("", key="SearchedMovies", options=sorted(list(st.session_state['options'])), default=st.session_state['default_value'], max_selections=5,label_visibility='collapsed', disabled=st.session_state.disabled_selectBar)
         if len(self.selectbar)>0:
             st.session_state.default.update(st.session_state.SearchedMovies)
 
@@ -65,34 +85,124 @@ class movie_counsel:
             st.session_state.recommended_movies_index = None
             st.session_state.movie_details_selected = None
             st.session_state.movie_details_recommended = None
-            st.session_state.recom_movie_details = None 
+            st.session_state.recom_movie_details = None
+            st.session_state.filter_df = st.session_state.movies_df
+            st.session_state.cntry_default = None
+            st.session_state.cntry_list = set()
+            st.session_state.genre_default = None
+            st.session_state.genre_list = set()
+            st.session_state.language_default = None
+            st.session_state.language_list = set()
+            st.session_state.kind_default = None
+            st.session_state.kind_list = set()
+            st.session_state.result_list = set()
+            st.session_state.result_default = None
+            st.session_state.apply_button = False
+            st.session_state.disabled_selectBar = False
+            st.session_state.disabled_inputBar = False
 
     def create_resetButton(self):
         ''' This method creates a Reset Button. It will reset all the session_state variables'''
-        self.reset_button = st.button("Reset", key="reset_button", type='primary', on_click=self.todo_on_resetButton_clicked)
+        self.reset_button = st.button("Reset", key="reset_button", type='primary', on_click=self.todo_on_resetButton_clicked, use_container_width=True)
     
-    def fetch_movie_details(self, movies_df,index_provided = False):
+    def todo_on_toogle_change(self):
+        if st.session_state.toogleButton:
+            st.session_state.disabled_selectBar = True
+            st.session_state.disabled_inputBar = True
+            # st.session_state.default = set()
+            # st.session_state.default_value = None
+
+        else:
+            st.session_state.disabled = False
+            st.session_state.disabled_selectBar = False
+            st.session_state.disabled_inputBar = False
+
+    def create_toogle(self):
+        self.toogle_button = st.toggle(":red[&#9776;]", key="toogleButton", help="Filter Movies", on_change=self.todo_on_toogle_change)
+
+    def todo_on_country_change(self):
+        if len(st.session_state.filterCountry)>0:
+            st.session_state.cntry_list.update(st.session_state.filterCountry)
+            st.session_state.cntry_default = st.session_state.cntry_list
+    
+    def todo_on_genre_change(self):
+        if len(st.session_state.filterGenre)>0:
+            st.session_state.genre_list.update(st.session_state.filterGenre)
+            st.session_state.genre_default = st.session_state.genre_list
+
+    def todo_on_kind_change(self):
+        if len(st.session_state.filterKind)>0:
+            st.session_state.kind_list.update(st.session_state.filterKind)
+            st.session_state.kind_default = st.session_state.kind_list
+
+    def todo_on_language_change(self):
+        if len(st.session_state.filterLanguage)>0:
+            st.session_state.language_list.update(st.session_state.filterLanguage)
+            st.session_state.language_default = st.session_state.language_list
+
+    def todo_on_resultList_change(self):
+        if len(st.session_state.resultList)>0:
+            st.session_state.result_list.update(st.session_state.resultList)
+            st.session_state.result_default = st.session_state.result_list
+            st.session_state.options.update(st.session_state.result_list)
+            st.session_state.default.update(st.session_state.result_list)
+
+    def todo_on_applyButton_click(self):
+        st.session_state.apply_button = st.session_state.applyButton
+
+    def show_filters(self, np):
+        if st.session_state.toogleButton:
+            with st.expander("Filter out as per your preferance", expanded=True):
+                col1, col2 = st.columns(2)
+                col3, col4 = st.columns(2)
+                cntrySet = set()
+                for x in st.session_state.movies_df.country.unique().tolist():
+                    cntrySet.update(x.split(','))
+                selected_cntry_list = col1.multiselect("Country", key="filterCountry", options=sorted(list(cntrySet)), default= st.session_state.cntry_default, on_change=self.todo_on_country_change)
+               
+                genreSet = set()
+                for x in st.session_state.movies_df.genre.unique().tolist():
+                    genreSet.update(x.split(','))
+                selected_genre_list = col2.multiselect("Genre", key="filterGenre", options=sorted(list(genreSet)), default= st.session_state.genre_default, on_change=self.todo_on_genre_change)  
+                selected_kind_list = col3.multiselect("Kind", key="filterKind", options=np.sort(st.session_state.movies_df.kind.unique()), default= st.session_state.kind_default, on_change=self.todo_on_kind_change)
+                selected_language_list = col4.multiselect("Language", key="filterLanguage", options=np.sort(st.session_state.movies_df.languages.unique()), default= st.session_state.language_default, on_change=self.todo_on_language_change)
+
+                if len(selected_cntry_list)!=0 or len(selected_genre_list)!=0 or len(selected_kind_list)!=0 or len(selected_language_list)!=0:
+                    apply_button = st.button("Apply", key="applyButton", type='primary', on_click=self.todo_on_applyButton_click)
+                    if st.session_state.apply_button:
+                        if len(selected_cntry_list)!=0:
+                            st.session_state.filter_df = st.session_state.movies_df[st.session_state.movies_df.country.str.contains('|'.join(selected_cntry_list))]
+                        if len(selected_genre_list)!=0:
+                            st.session_state.filter_df = st.session_state.filter_df[st.session_state.filter_df.genre.str.contains('|'.join(selected_genre_list))]
+                        if len(selected_kind_list)!=0:
+                            st.session_state.filter_df = st.session_state.filter_df[st.session_state.filter_df.kind.str.contains('|'.join(selected_kind_list))]
+                        if len(selected_language_list)!=0:
+                            st.session_state.filter_df = st.session_state.filter_df[st.session_state.filter_df.languages.str.contains('|'.join(selected_language_list))]
+    
+                        st.multiselect("",options=st.session_state.filter_df.name, key="resultList", default=st.session_state.result_default, max_selections=5, on_change=self.todo_on_resultList_change)
+
+    def fetch_movie_details(self, index_provided = False):
         ''' If input value index_provided = False(default) then this method will fetch user's selected movies from movies dataset based on movie names and store it in a list of dictionaries. Each dictionary contains details of one movie.
             If index_provided = True, then this method will fetch details of recommended movies from movies dataset based on indexes provided by the recommend method and store it in a list of dictionaries. Each dictionary contains details of one movie.
               '''
         if not index_provided:
             st.session_state.movie_details_selected = []
             for movie in st.session_state.default:
-                    index = movies_df[movies_df.name == movie].index[0]
+                    index = st.session_state.movies_df[st.session_state.movies_df.name == movie].index[0]
                     temp = []
-                    for i in range(movies_df.columns.shape[0]):
-                        temp.append(movies_df.iloc[index, i])
+                    for i in range(st.session_state.movies_df.columns.shape[0]):
+                        temp.append(st.session_state.movies_df.iloc[index, i])
                     temp.append(index)
-                    col_names = movies_df.columns.tolist()
+                    col_names = st.session_state.movies_df.columns.tolist()
                     col_names.append('index')
                     st.session_state.movie_details_selected.append(dict(zip(col_names, temp)))
         else:
             st.session_state.movie_details_recommended = []
             for index in st.session_state.recommended_movies_index:
                 temp = []
-                for i in range(movies_df.columns.shape[0]):
-                    temp.append(movies_df.iloc[index, i])
-                col_names = movies_df.columns.tolist()
+                for i in range(st.session_state.movies_df.columns.shape[0]):
+                    temp.append(st.session_state.movies_df.iloc[index, i])
+                col_names = st.session_state.movies_df.columns.tolist()
                 st.session_state.movie_details_recommended.append(dict(zip(col_names, temp)))
     
     def create_movie_grid(self, n=5):
@@ -199,6 +309,7 @@ class movie_counsel:
                     <div class="rect_box">
                         <center><h4 style="font-weight:bold;font-family:rubik;color:#FF3A4A;">Info</h4></center>
                         <p>Type - {movie['kind']}</p>
+                         <p>Year - {movie['year']}</p>
                         <p>Rating - {movie['rating']}🌟</p>
                         <p>Genre - {movie['genre']}</p>
                         <p>Runtime - {runtime}</p>
@@ -215,20 +326,21 @@ class movie_counsel:
         """
         st.markdown(containers, unsafe_allow_html=True)
 
-    def create_inputBar_selectBar_resetButton(self,movies_df):
+    def create_inputBar_selectBar_resetButton(self):
         ''' This method will call methods to create text_input label, multiselect label and reset button, align them with in a single row using st.columns'''
         
-        cols = self.create_n_columns([2,7,1])
+        cols = self.create_n_columns([2,6,1,1])
         with cols[0]:
-            self.create_textInput(movies_df)
-            # movie_counsel_obj.set_selectBar_options_as_per_inputMovie()
+            self.create_textInput()
         with cols[1]:
             self.create_selectbar()
         with cols[2]:
+            self.create_toogle()
+        with cols[3]:
             self.create_resetButton()
 
-    def recommend(self,similarity, n=5):
-        ''' this method will recommend movies based on user's selected movies using the similarity matrix.
+    def recommend(self, n=5):
+        ''' this method will recommend movies based on user's selected movies using the st.session_state.similarity matrix.
             it will provide the indexes of the recommended movies '''
         
         index_list = [x['index'] for x in st.session_state.movie_details_selected]
@@ -237,7 +349,7 @@ class movie_counsel:
             n = 10
         for x in index_list:
             count = 0
-            distances = similarity[x]
+            distances = st.session_state.similarity[x]
             movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:n*4]
             # print(movie_list);print(movie_list[0]);print(movie_list[0][0]);break
             for i in range(len(movie_list)):
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
 # =========================== Imporitng required libraries ==============================================================================================
 import streamlit as st
 import pandas as pd
+import numpy as np
 import pickle
 # import joblib
 from urllib.request import urlopen
 from movie_counsel import *
 # =========================== Fetching dataset and ML Model ==============================================================================================
-    # using st.cache_resource will run globally for the first time only. for all the sessions later results will fetched from cache.
-
-@st.cache_resource
+    
+@st.cache_resource   # using st.cache_resource will run globally for the first time only. for all the sessions later results will fetched from cache.
 def import_dataset_similarity_matrix():
     ''' This function will fetch and return pandas, numpy object for dataset and the ML Model respectively.
         Note:- Make sure this function is using streamlit's cache properties to avoid loading datasets on every refresh '''
@@ -20,21 +20,24 @@ def import_dataset_similarity_matrix():
     similarity = pickle.load(urlopen(g_drive_url_pkl)) # loading similarity matrix for Recommendation
     
     return movies_df, similarity
-# ================================ Calling class movie_counsel ot build web app ====================================================================================
 
+
+# ================================ Calling class movie_counsel ot build web app ====================================================================================
 if __name__ == '__main__':
-  
+    
     movie_counsel_obj = movie_counsel() # creating object
     movies_df, similarity = import_dataset_similarity_matrix() # importing datasets, ML Model
+    movie_counsel_obj.set_source_data(movies_df, similarity)
     movie_counsel_obj.set_logo()
     movie_counsel_obj.set_title("Your Movie counseller")
-    movie_counsel_obj.create_inputBar_selectBar_resetButton(movies_df)
-
+    movie_counsel_obj.create_inputBar_selectBar_resetButton()
+    movie_counsel_obj.show_filters(np)
+    
     if(len(st.session_state.default)>0):
         movie_counsel_obj.set_title("Selected Movies")
-        movie_counsel_obj.fetch_movie_details(movies_df, index_provided=False) # get all movie deatails for user's selected movies
+        movie_counsel_obj.fetch_movie_details(index_provided=False) # get all movie deatails for user's selected movies
         movie_counsel_obj.create_movie_grid(5) # show user's selected movies's cover in 5*5 grid
         movie_counsel_obj.set_title("Recommendations")
-        movie_counsel_obj.recommend(similarity, n=5)
-        movie_counsel_obj.fetch_movie_details(movies_df, index_provided=True) # get all movie deatails for recommended movies
+        movie_counsel_obj.recommend(n=5)
+        movie_counsel_obj.fetch_movie_details(index_provided=True) # get all movie deatails for recommended movies
         movie_counsel_obj.display_recommended_movie_details()
